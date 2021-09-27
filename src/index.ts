3c35e737884318d24020b00f2aea4bea9ad59ecd
Kan Luo <lkklcozy@gmail.com>
2021-09-26 23:49:06 -0600

    refactor: update process check

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
 import * as core from '@actions/core'
-import puppeteer from 'puppeteer'
+import puppeteer, { Page } from 'puppeteer'
 
 const saveData = (key: string, value: string) => {
   core.setOutput(key, value)
   core.exportVariable(key, value)
 }
+
+const selectors = [
+  {
+    title:
+      'body > main > div:nth-child(2) > div:nth-child(8) > div > div > div:nth-child(1) > h3',
+    content:
+      'body > main > div:nth-child(2) > div:nth-child(8) > div > div > div.mwspanel.section > div > div > div.reference.parbase.section > div > div > p',
+    current:
+      'body > main > div:nth-child(2) > div:nth-child(3) > div > div > div.reference.parbase.section > div > div > p',
+  },
+  {
+    title: '#canada-online',
+    content:
+      'body > main > div:nth-child(2) > div:nth-child(20) > div > div > div.mwspanel.section > div > div > div.reference.parbase.section > div > div > p',
+  },
+  {
+    title:
+      'body > main > div:nth-child(2) > div:nth-child(3) > div > div > div:nth-child(3) > h4',
+    content:
+      'body > main > div:nth-child(2) > div:nth-child(3) > div > div > div.mwspanel.section > div > div > div.reference.parbase.section > div > div > p',
+  },
+]
+
+const getContent = async (page: Page, selector: string) =>
+  page.$eval(selector, (el: Element) => {
+    return (el as HTMLElement)?.innerText.replace('?', '').trim()
+  })
 
 ;(async () => {
   const browser = await puppeteer.launch()
@@ -16,26 +43,20 @@ const saveData = (key: string, value: string) => {
       waitUntil: 'networkidle0',
     },
   )
-  const continueButton = await page.$('aria/Continue')
-  if (continueButton) {
-    await continueButton.click()
-  }
 
-  const latest = await page.$eval('.label.label-info', el =>
-    (el as HTMLElement).innerText.replace('Last updated:', ''),
+  const results = await Promise.all(
+    selectors.map(async selector => {
+      const { title, content, current } = selector
+      const t = await getContent(page, title)
+      const c = await getContent(page, content)
+      const cur = current ? await getContent(page, current) : ''
+      if (cur) {
+        saveData('current', cur.replace('Last updated:', ''))
+      }
+      return `<h1>${t}</h1><br/ ><p>${c}</p>`
+    }),
   )
-  const current = await page.$eval('.mrgn-tp-0.h2', el =>
-    (el as HTMLElement).innerText.replace(/\n/g, '').replace('?', ''),
-  )
 
-  const hasNewData =
-    latest !== process.env.latest || current !== process.env.current
-
-  if (hasNewData) {
-    saveData('latest', latest)
-    saveData('current', current)
-  }
-
-  core.setOutput('hasNewData', hasNewData)
+  saveData('content', results.join())
   await browser.close()
 })()
