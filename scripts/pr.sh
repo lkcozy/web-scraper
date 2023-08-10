f177a340bb37f0d81d281bf5e66870610fecd1ae
lkcozy <lkklcozy@gmail.com>
2023-08-10 14:51:01 -0600

    refactor: update prs log workflow

--- scripts/pr.sh
+++ scripts/pr.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-pr_log_name="pullrequestlog.md"
+pr_log_name="PULLREQUESTLOG.md"
 temp_file_name="temp"
-start_datetime="2023-08-03T11:38:25Z"
+start_datetime="2020-08-03T11:38:25Z"
 base="main"
 search_query="is:pr merged:>$start_datetime -author:app/dependabot"
 merged_prs=$(
     gh pr list \
         --base $base \
         --search "$search_query" \
-        --json author,title,url,mergedAt \
-        --jq '.[]'
+        --json author,title,url,mergedAt
+    # --jq ".[]"
 )
 echo "$merged_prs"
 
@@ -19,6 +19,8 @@ if [ ! -f $pr_log_name ]; then
 fi
 
 if [ -n "$merged_prs" ]; then
+    prs=$(echo "$merged_prs" | jq -c '.[]')
+    echo "$prs"
     echo -e "$(date +'## %Y-%m-%d (Week %V)')\n" >$temp_file_name
     counter=1
     while read -r line; do
@@ -27,7 +29,7 @@ if [ -n "$merged_prs" ]; then
         url=$(echo "$line" | jq -r '.url')
         echo "$counter. [$title]($url) @$author" >>$temp_file_name
         ((counter++))
-    done <<<"$merged_prs"
+    done <<<"$prs"
 
     echo -e "" >>$temp_file_name
     cat $pr_log_name >>$temp_file_name
